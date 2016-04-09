fc77bc339f33f198b7bad8adab4719c52be8e69c
Carlos Alfredo Osorio Perez <hacybeyker@hotmail.com>
2016-04-08 23:23:05 -0500

    agregamos producto

---
+++ demo01git/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo01git.Models
+{
+    public class Producto
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public double precio { get; set; }
+
+    }
+}
